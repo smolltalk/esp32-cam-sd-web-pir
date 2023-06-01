e3f5d91e881bacf5ca4a9525a275d89984f8f713
smolltalk <morvan.sebastien@gmail.com>
2023-06-01 22:54:34 +0200

    Clean config.h

--- config.h
+++ config.h
@@ -1,29 +1,11 @@
-// Home
-// #define WIFI_SSID "Livebox-BD50"
-// #define WIFI_PASSWORD ""
-// #define WIFI_CONNECT_MAX_ATTEMPTS 20
-
-// #define UPLOAD_SERVER_NAME String("catchbirdpic.000webhostapp.com")
-// #define UPLOAD_SERVER_PORT 80
-// #define UPLOAD_SERVER_PATH String("/upload.php")
-
-// MakerSpace56 Fablab
-// #define WIFI_SSID "Invite"
-// #define WIFI_PASSWORD "V@nnes56##"
-// #define WIFI_CONNECT_MAX_ATTEMPTS 30
-
-// #define UPLOAD_SERVER_NAME String("catchbirdpic.000webhostapp.com")
-// #define UPLOAD_SERVER_PORT 80
-// #define UPLOAD_SERVER_PATH String("/upload.php")
-
 #define SAVE_PICTURE_ON_SD_CARD 1
 #define UPLOAD_PICTURE 0
 
-#define WIFI_SSID "AndroidAP7450"
-#define WIFI_PASSWORD "menimur56"
+#define WIFI_SSID "MyWiFi"
+#define WIFI_PASSWORD "MyPassword"
 #define WIFI_CONNECT_MAX_ATTEMPTS 30
 
-#define UPLOAD_SERVER_NAME "catchbirdpic.000webhostapp.com"
+#define UPLOAD_SERVER_NAME ""
 #define UPLOAD_SERVER_PORT 80
 #define UPLOAD_SERVER_PATH "/upload.php"
 
